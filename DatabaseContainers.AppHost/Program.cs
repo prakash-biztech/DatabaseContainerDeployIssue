44c5ba37619794684b3c6210a3ece1b504948db4
Prakash <prakash.b@biztechcs.com>
2025-05-09 09:49:14 +0530

    Initial commit

---
+++ DatabaseContainers.AppHost/Program.cs
@@ -0,0 +1,27 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+// PostgreSQL container is configured with an auto-generated password by default
+// and supports setting the default database name via an environment variable & running *.sql/*.sh scripts in a bind mount.
+var todosDbName = "Todos";
+
+var postgres = builder.AddPostgres("postgres")
+    // Set the name of the default database to auto-create on container startup.
+    .WithEnvironment("POSTGRES_DB", todosDbName)
+    // Mount the SQL scripts directory into the container so that the init scripts run.
+    .WithBindMount("../DatabaseContainers.ApiService/data/postgres", "/docker-entrypoint-initdb.d")
+    // Configure the container to store data in a volume so that it persists across instances.
+    .WithDataVolume()
+    .WithPgWeb()
+    // Keep the container running between app host sessions.
+    .WithLifetime(ContainerLifetime.Persistent);
+
+// Add the default database to the application model so that it can be referenced by other resources.
+var todosDb = postgres.AddDatabase(todosDbName);
+
+
+
+builder.AddProject<Projects.DatabaseContainers_ApiService>("apiservice")
+    .WithReference(todosDb)
+    .WaitFor(todosDb);
+
+builder.Build().Run();
